2721f61dca8b776f717f83c9321d3ae911e3139d
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-11-10 17:27:58 +0700

    membuat method addnote

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -9,7 +9,7 @@ namespace Singly_linked_list
     class Node
     {
         public int noMhs;
-        public int nama;
+        public string nama;
         public Node next;
     }
     class List
@@ -21,7 +21,48 @@ namespace Singly_linked_list
         }
         //Method untuk menambahkan sebuah node ke dalam list
 
-        
+        public void addnote()
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nMasukan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nMasukan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            //Node ditambahkan sebagai node pertama 
+            if(START == null || nim <= START.noMhs)
+            {
+                if((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diizinkan ");
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+            //Menemukan lokasi node baru didalam list
+            Node previus, currrent;
+            previus = START;
+            currrent = START;
+
+            while((currrent != null) && (nim >= currrent.noMhs))
+            {
+                if(nim == currrent.noMhs)
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diizinkan");
+                    return;
+                }
+                previus = currrent;
+                currrent = currrent.next;
+            }
+            nodeBaru.next = currrent;
+            previus.next = nodeBaru;
+        }
+        //Methodw
     }
     class Program
     {
